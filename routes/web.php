5d60ff4e3ddb6d59fb293dbbecb18fd599b744b5
Edwin <edwincas1012@gmail.com>
2026-06-19 23:57:31 -0300

    Configuração do banco e conexão

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Site\SiteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home', [SiteController::class, 'index']);
