236c551fd99c4c19600e7a906fda2b97e1c95f9c
Edwin <edwincas1012@gmail.com>
2026-06-22 22:14:19 -0300

    add:configuração de rotas iniciais

---
+++ resources/views/site/support/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Suporte</title>
+</head>
+<body>
+    <h1>Pagina de Suporte</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Site\SiteController;
+use App\Http\Controllers\Site\SupportController;
 
 Route::get('/home', [SiteController::class, 'index']);
+
+
+Route::get('/forum', [SupportController::class, 'index']);
